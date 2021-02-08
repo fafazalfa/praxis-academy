3118bc3a6cd813f35208e0c509eca48cbc999a8c
fafazalfa <zalfafarhah0802@gmail.com>
2021-02-08 15:06:18 +0700

    hari 1

---
+++ novice/01-01/kasus/main.js
@@ -0,0 +1,7 @@
+ let identitas="Nama= zalfa <p> Alamat = Purwobinangun <p> pendidikan terakhir = SMA TRENSAINS";
+ console.log(identitas);
+ document.getElementById("oke").innerHTML=identitas;
+
+
+
+
---
+++ novice/01-01/latihan/main.js
@@ -0,0 +1,19 @@
+let five =5;
+console.log(five*five);
+
+let mood ="light";
+console.log(mood);
+
+mood= "drak";
+console.log(mood);
+
+let one=1, two=2;
+console.log(one + two);
+
+var Name = " Ayda";
+const greeting = " Hello ";
+console.log(greeting + Name );
+
+let cought= 10 * 5;
+console.log(cought);
+
