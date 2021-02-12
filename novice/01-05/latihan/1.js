39c03fe860acfa5df12e28f35ed129726383c596
fafazalfa <zalfafarhah0802@gmail.com>
2021-02-12 16:28:33 +0700

    hari 5

---
+++ novice/01-05/latihan/1.js
@@ -0,0 +1,6 @@
+let oi = new object ()
+oi.foo = 42
+console.log(oi)
+//object {foo:42}
+
+
---
+++ novice/01-05/latihan/2.js
@@ -0,0 +1,8 @@
+const map1 = new map ();
+map1.set ('bar','foo');
+
+
+console.log (map1.delete('bar'));
+
+console.log(map1.has('bar'));
+
