19af737799255e2dcf78e3047da0b1c69e9101d8
fafazalfa <zalfafarhah0802@gmail.com>
2021-02-19 09:21:22 +0700

    hari9

---
+++ novice/02-04/latihan/main.js
@@ -0,0 +1,30 @@
+console.log("selamat pagi");
+document.getElementById("oke").innerHTML="Hallo dunia"
+
+function tambah (a , b) {
+  console.log(a+b);
+  function oke () {
+    console.log ("halo");
+  }
+  oke ();
+}
+
+// es5 var
+// es6 let and const 
+var first_name; // deklarasi variable 
+first_name = "fafa" // definisi variable, pengisisan nilai variable
+console.log(first_name);
+
+var num = 33;
+console.log(num);
+
+var benar = false;
+console.log(benar);
+
+
+let last_name = "ningrum";
+console.log(last_name);
+
+
+document.getElementById("oke").innerHTML = first_name;
+
